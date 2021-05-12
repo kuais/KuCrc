aab79fbc51354221224d626fa31326439b10a9a3
kuais <dlm321@126.com>
2021-05-12 13:49:28 +0800

    添加项目文件。

---
+++ KuCrcTests/KuCrcTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace Ku.Tests
+{
+    [TestClass()]
+    public class KuCrcTests
+    {
+        [TestMethod()]
+        public void InitTableTest()
+        {
+            KuCrc crc = new KuCrc(KuCrc.CrcType.CRC32);
+            crc.InitTable();
+        }
+
+        [TestMethod()]
+        public void CrcTest()
+        {
+            byte[] input = Encoding.ASCII.GetBytes("1234567");
+            KuCrc crc = new KuCrc(KuCrc.CrcType.CRC32);
+            //KuCrc crc = new KuCrc();
+            //crc.config.Poly = 0x1021;
+            //crc.config.Offset = 0x1D0F;
+            //crc.config.XorOut = 0x1234;
+            //crc.config.Offset = 0x0000;
+            //crc.config.ReflectIn = false;
+            //crc.config.ReflectOut = false;
+            byte[] vs = crc.ComputeHash(input);
+        }
+    }
+}
